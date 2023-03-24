800f797d54427c31baac0eadb4aa3f33508acc83
NA anna <laanna1001@gmail.com>
2023-03-24 17:31:49 +0900

    commit

--- src/store/movie.js
+++ src/store/movie.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
-export default ({
+const API_KEY = '32e321fd'
+
+export default {
     // 현재 파일(movie.js)을 Store 모듈로 활용하려면 다음 옵션이 필요합니다.
     namespaced: true,
 
@@ -22,29 +24,61 @@ export default ({
             .forEach(key => {
                 state[key] = payload[key]
             })
+        },
+        pushIntoMovies (state, movies) {
+            state.movies.push(...movies)
         }
     },
 
     // Actions의 주요 목적은 Mutations를 실행시키는 역할
     // dispatch 로 실행
     actions: {
-        async searchMovies ({ commit, state }) {
+        function fetchMovies ({ state, commit }, pageNum) {
+            const promise = new Promise(resolve => {
+              const res = axios.get(`https://omdbapi.com/?apikey=${API_KEY}&s=${state.title}&page=${pageNum}`)
+              resolve(res)
+            })
+            return promise.then(res => res.data)
+          },
+        // fetchMovies ({ state, commit }, pageNum) {
+        //     return new Promise( async resolve => {
+        //         const res = await axios.get(`https://omdbapi.com/?apikey=${API_KEY}&s=${state.title}&page=${pageNum}`)
+        //         commit('pushIntoMovies', res.data.Search )
+        //         resolve(res.data)
+        //     })
+        // },
+        async searchMovies ({ commit, dispatch }) {
         
             commit('updateState', {
                 loading: true, // 로딩 애니메이션 시작
                 movies: [] // 초기화
             })
-            
-            const res = await axios.get(`https://omdbapi.com/?apikey=32e321fd&page=1&s=${state.title}`)
-            console.log(res)
+            const { totalResults } = await dispatch('fetchMovies', 1)  
+            const pageLength = Math.ceil(totalResults / 10)
+
+            // const res = await axios.get(`https://omdbapi.com/?apikey=32e321fd&s=${state.title}&page=1`)
+            // console.log(res)
+            // const pageLength = Math.ceil(res.data.totalResults / 10)
+
+            // commit('updateState', {
+            //     movies: res.data.Search
+            // })
+
+            if ( pageLength > 1 ) {
+                for (let i = 2 ; i <= pageLength; i += 1) {
+                    if ( i > 4 ) break
+                    await dispatch('fetchMovies', i)  
+                    // const resMore = await axios.get(`https://omdbapi.com/?apikey=32e321fd&s=${state.title}&page=1${i}`)                    
+                    // commit('pushIntoMovies', resMore.data.Search )
+                }
+            }
             
             commit('updateState', {
                 loading: false, // 로딩 애니메이션 종료
-                movies: res.data.Search
             })
         }
     },
 
     modules: {
     }
-  })
+  }
