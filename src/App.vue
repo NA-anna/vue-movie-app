10296e4fc2651d136e85f8d3a235ef57b8079d7e
naanna <yujin.na@waycen.com>
2023-04-03 18:09:37 +0900

    commit

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,10 @@
       <SearchBar/> <!-- 검색바 -->
       <MovieList/> <!-- 영화목록 -->
     </v-container>
-    <v-main>
-      <HelloWorld/>
-    </v-main>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import SearchBar from '@/components/SearchBar.vue' //@ : 절대경로
 import MovieList from '@/components/MovieList.vue'
 
@@ -19,7 +15,6 @@ export default {
   name: 'App',
 
   components: {
-    HelloWorld,
     SearchBar,
     MovieList,
   },
